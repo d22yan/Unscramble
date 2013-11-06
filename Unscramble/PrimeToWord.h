05b32495e8705e05941ab4fce3e253df6bb7bcb5
Danny Yan <yan4646@hotmail.com>
2013-11-05 22:13:25 -0500

    Refactored PrimeToWord, Fully migrated the remaining members and methods

--- Unscramble/PrimeToWord.h
+++ Unscramble/PrimeToWord.h
@@ -10,6 +10,8 @@ class PrimeToWord {
 	public:
 		map_mapm_liststring PrimeWordDictionary;
 		static PrimeToWord* GetInstance();
+		void InsertPrimeWord(std::string);
+		static MAPM WordToPrime(std::string);
 };
 
 bool PrimeToWord::instanceFlag = false;
@@ -27,6 +29,39 @@ PrimeToWord* PrimeToWord::GetInstance() {
 		return single;
 	}
 	return single;
+}
+
+MAPM PrimeToWord::WordToPrime(std::string word) {
+	CommonAlphabet *CommonAlphabetInstance = CommonAlphabet::GetInstance();
+	MAPM prime = 1;
+	std::string tolowercase = Utility::ToLowerCase(word);
+	std::vector<char> characters(tolowercase.begin(), tolowercase.end());
+	for(std::vector<char>::iterator iterator = characters.begin(), end = characters.end(); iterator != end; ++iterator) {
+		prime *= CommonAlphabetInstance->PrimeCommonAlphabetDictionary.at(*iterator);
+	}
+	return prime;
+}
+
+void PrimeToWord::InsertPrimeWord(std::string word) {
+	bool valid = true;
+	int WordLength = word.length();
+	for (std::string::iterator iterator = word.begin(), end = word.end(); iterator != end; ++iterator) {
+		if (!isalpha(*iterator)) {
+			valid = false;
+		}
+	}
+	if (valid) {
+		std::string tolowercase = Utility::ToLowerCase(word);
+		MAPM prime = WordToPrime(tolowercase);
+		if (PrimeWordDictionary.find(prime) == PrimeWordDictionary.end()) {
+			std::list<std::string> NewList;
+			NewList.insert(NewList.begin(),tolowercase);
+			PrimeWordDictionary.insert(pair_mapm_liststring(WordToPrime(tolowercase),NewList));
+		}
+		else {
+			PrimeWordDictionary.at(prime).insert(PrimeWordDictionary.at(prime).begin(),tolowercase);
+		}
+	}
 }
 
 #endif
