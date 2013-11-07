4d630af0286c2c9853f03786948c810a4f451791
Danny Yan <yan4646@hotmail.com>
2013-11-06 22:54:59 -0500

    Added reading PrimeDictionary method.

--- Unscramble/PrimeWordList.h
+++ Unscramble/PrimeWordList.h
@@ -3,26 +3,41 @@
 
 class PrimeWordList {
 	public:
+		static void ConvertToPrimeDictionary(std::string);
 		static void ReadPrimeDictionary();
 };
 
-void PrimeWordList::ReadPrimeDictionary() {
-	std::ifstream StreamFile;
-	StreamFile.open(DictionaryFileName);
-	if (StreamFile.is_open()) {
+void PrimeWordList::ConvertToPrimeDictionary(std::string input) {
+	std::string	DictioanryFileName = input;
+	std::fstream IOStreamFile(input);
+	if(IOStreamFile.is_open()) {
 		std::string Line;
-		while(!StreamFile.eof()) {
-			StreamFile >> Line;
+		while(!IOStreamFile.eof()) {
+			IOStreamFile >> Line;
+		}
+	}
+	IOStreamFile.close();
+}
+
+void PrimeWordList::ReadPrimeDictionary() {
+	std::ifstream ReadStreamFile;
+	ReadStreamFile.open(DictionaryFileName);
+	if (ReadStreamFile.is_open()) {
+		std::string Line;
+		while(!ReadStreamFile.eof()) {
+			ReadStreamFile >> Line;
 			std::vector<std::string> SplitLine;
 			boost::split(SplitLine, Line, ::isspace);
 			if (SplitLine.size() == 2) {
-				MAPM PrimeNumber = "123456"; // Convert String to MAPM
+				std::string PrimeString = SplitLine[0];
 				std::string Word = SplitLine[1];
+				const char* PrimeChars = PrimeString.c_str();
+				MAPM PrimeNumber = PrimeChars;
 				PrimeToWord::GetInstance()->InsertPrime(PrimeNumber, Word);
 			}
 		}
 	}
-	StreamFile.close();
+	ReadStreamFile.close();
 }
 
 #endif
