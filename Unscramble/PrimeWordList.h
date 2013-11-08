fae2dbc1a40553a4cbc1514671050891c86bf622
Danny Yan <yan4646@hotmail.com>
2013-11-07 22:07:25 -0500

    Created a PrimedDictionary converter.

--- Unscramble/PrimeWordList.h
+++ Unscramble/PrimeWordList.h
@@ -3,41 +3,51 @@
 
 class PrimeWordList {
 	public:
-		static void ConvertToPrimeDictionary(std::string);
+		static std::string ConvertToPrimeDictionary(std::string);
 		static void ReadPrimeDictionary();
 };
 
-void PrimeWordList::ConvertToPrimeDictionary(std::string input) {
+std::string PrimeWordList::ConvertToPrimeDictionary(std::string input) {
 	std::string	DictioanryFileName = input;
-	std::fstream IOStreamFile(input);
-	if(IOStreamFile.is_open()) {
+	std::string NewDictionaryFileName = "primed" + input;
+	std::ifstream IStreamFile(input);
+	std::ofstream OStreamFile(NewDictionaryFileName);
+	if(IStreamFile.is_open() && OStreamFile.is_open()) {
 		std::string Line;
-		while(!IOStreamFile.eof()) {
-			IOStreamFile >> Line;
+		while(!IStreamFile.eof()) {
+			IStreamFile >> Line;
+			MAPM Prime = PrimeToWord::WordToPrime(Line);
+			char PrimeString[MAXIMUM_DIGIT];
+			Prime.toIntegerString(PrimeString);
+			std::string NewLine(PrimeString);
+			NewLine.append(" " + Line);
+			OStreamFile << NewLine << std::endl;
 		}
 	}
-	IOStreamFile.close();
+	IStreamFile.close();
+	OStreamFile.close();
+	return NewDictionaryFileName;
 }
 
 void PrimeWordList::ReadPrimeDictionary() {
-	std::ifstream ReadStreamFile;
-	ReadStreamFile.open(DictionaryFileName);
-	if (ReadStreamFile.is_open()) {
+	std::ifstream IStreamFile;
+	IStreamFile.open(DictionaryFileName);
+	if (IStreamFile.is_open()) {
 		std::string Line;
-		while(!ReadStreamFile.eof()) {
-			ReadStreamFile >> Line;
+		while(!IStreamFile.eof()) {
+			IStreamFile >> Line;
 			std::vector<std::string> SplitLine;
 			boost::split(SplitLine, Line, ::isspace);
 			if (SplitLine.size() == 2) {
 				std::string PrimeString = SplitLine[0];
 				std::string Word = SplitLine[1];
 				const char* PrimeChars = PrimeString.c_str();
-				MAPM PrimeNumber = PrimeChars;
-				PrimeToWord::GetInstance()->InsertPrime(PrimeNumber, Word);
+				MAPM Prime = PrimeChars;
+				PrimeToWord::GetInstance()->InsertPrime(Prime, Word);
 			}
 		}
 	}
-	ReadStreamFile.close();
+	IStreamFile.close();
 }
 
 #endif
--- Unscramble/main.cpp
+++ Unscramble/main.cpp
@@ -2,6 +2,7 @@
 // slow construction of the dictionary, possibly avoid the usage of iostream
 // refactor read file http://stackoverflow.com/questions/7868936/c-read-file-line-by-line
 // generate prime word list
+// check input feed contains correct format and requirements (PrimeWordList requires check on input if it is word)
 
 #include <algorithm>
 #include <fstream> 
@@ -17,7 +18,8 @@
 #include <boost\dynamic_bitset\dynamic_bitset.hpp>
 #include <M_APM.H> // http://www.tc.umn.edu/~ringx004/mapm-main.html
 
-#define NUMBER_OF_LETTERS 26
+#define ALPHABET_COUNT 26
+#define MAXIMUM_DIGIT 256
 const std::string DictionaryFileName = "dictionary2.txt";
 typedef std::map<MAPM,std::list<std::string>> map_mapm_liststring;
 typedef std::pair<MAPM,std::list<std::string>> pair_mapm_liststring;
